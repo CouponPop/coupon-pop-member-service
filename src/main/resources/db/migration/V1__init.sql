3532c714ac05339e868ca8e4666bcbf12a4524ce
seonrizee <seonrizee@gmail.com>
2025-10-30 15:41:15 +0900

    chore: flyway members 테이블 관련 스크립트 추가

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA member_db;
+
+CREATE TABLE members
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='회원 테이블';
