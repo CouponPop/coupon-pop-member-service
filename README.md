17f73f276c5b6a0ac5f97721084c8664cfd77166
seonrizee <seonrizee@gmail.com>
2025-10-30 11:20:29 +0900

    chore: project init

---
+++ README.md
@@ -0,0 +1,1 @@
+# couponpop-member
---
+++ src/main/java/com/couponpop/member/CouponpopMemberApplication.java
@@ -0,0 +1,13 @@
+package com.couponpop.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponpopMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponpopMemberApplication.class, args);
+    }
+
+}
